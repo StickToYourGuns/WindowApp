a24940b2cc49931b35cb340c878c2b90bd283063
Tim <60299355+StickToYourGuns@users.noreply.github.com>
2024-02-20 00:52:35 +0300

    5

--- script.js
+++ script.js
@@ -1,3 +1,287 @@
+let jsonAppl = {
+    "items": [{
+        "id": 0,
+        "name": "1",
+        "type": "2",
+        "location": "3",
+        "events": [{
+            "id": "0",
+            "name": "event 1",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "1",
+            "name": "event 1",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "2",
+            "name": "event 1",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "3",
+            "name": "event 1",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "4",
+            "name": "event 1",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        }
+        ]
+    },
+    {
+        "id": 1,
+        "name": "2",
+        "type": "2",
+        "location": "3",
+        "events": [{
+            "id": "0",
+            "name": "event 2",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "1",
+            "name": "event 2",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "2",
+            "name": "event 2",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        }
+        ]
+    },
+    {
+        "id": 2,
+        "name": "3",
+        "type": "2",
+        "location": "3",
+        "events": [{
+            "id": "0",
+            "name": "event 88",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "1",
+            "name": "event 5435",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "2",
+            "name": "event 12",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        }
+        ]
+    },
+    {
+        "id": 3,
+        "name": "4",
+        "type": "2",
+        "location": "3",
+        "events": [{
+            "id": "0",
+            "name": "event 3",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "1",
+            "name": "event 3",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "2",
+            "name": "event 3",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "1",
+            "name": "event 3",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "1",
+            "name": "event 3",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "1",
+            "name": "event 3",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        }
+        ]
+    },
+    {
+        "id": 4,
+        "name": "5",
+        "type": "2",
+        "location": "3",
+        "events": [{
+            "id": "0",
+            "name": "event 4",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "1",
+            "name": "event 4",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        },
+        {
+            "id": "2",
+            "name": "event 4",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        }
+        ]
+    },
+    {
+        "id": 5,
+        "name": "6",
+        "type": "2",
+        "location": "3",
+        "events": [{
+            "id": "0",
+            "name": "event 5",
+            "time": "05:17",
+            "I1": [100, 10],
+            "I2": [10000, 20],
+            "I3": [100, 10],
+            "U1": [10000, 20],
+            "U2": [100, 10],
+            "U3": [10000, 20]
+        }
+        ]
+    },
+    {
+        "id": 6,
+        "name": "7",
+        "type": "2",
+        "location": "3",
+        "events": []
+    }
+    ]
+}
+
 let menuStart = document.querySelector('.hideBar__start');
 let menuUl = document.querySelector('.hideBar__ul');
 let menuLi = document.querySelector('.hideBar__li');
@@ -20,18 +304,18 @@ class Window {
         this.name = "default window";
         this.window = document.createElement("div");
         this.window.classList.add("window");
-        
+
         this.controlPanel = document.createElement("div");
         this.controlPanel.classList.add("window__controlPanel");
-        
+
         this.title = document.createElement("p");
         this.title.classList.add("window__title");
         this.title.innerHTML = this.name;
-        
-        
+
+
         this.controls = document.createElement("div");
         this.controls.classList.add("window__controls");
-        
+
         this.button = document.createElement("div");
         this.button.classList.add("window__hide");
         handler(this.button, this, "hide");
@@ -45,31 +329,31 @@ class Window {
         handler(this.button, this, "close");
         this.controls.append(this.button);
         // потом переделаю в цикл
-        
+
         this.legend = document.createElement("div");
         this.legend.classList.add("window__legend");
-        
+
         this.content = document.createElement("div");
         this.content.classList.add("window__content");
-        
+
         this.window.append(this.controlPanel);
         this.window.append(this.legend);
         this.window.append(this.content);
         this.controlPanel.append(this.title);
         this.controlPanel.append(this.controls);
-        
+
         let areas = document.querySelectorAll(".workspace__area")
-        if ((areaNumber<0) || (areaNumber>2)) {
+        if ((areaNumber < 0) || (areaNumber > 2)) {
             areas[2].append(this.window);
             alert(`Элемент "${this.name}" автоматически установлен в конец`)
         } else {
-            for (let i=0; i<areas.length; i++) {
+            for (let i = 0; i < areas.length; i++) {
                 areas[areaNumber].append(this.window);
             }
         }
-        
-        function handler (target, obj, type) {
-            target.addEventListener("click", function(e) {
+
+        function handler(target, obj, type) {
+            target.addEventListener("click", function (e) {
                 e.stopPropagation();
                 if (type == "hide") {
                     obj.hide();
@@ -87,24 +371,24 @@ class Window {
         let window = this.window;
         window.classList.add("hidden");
         document.querySelector(".hideBar").append(window);
-        window.addEventListener("click", function(e) {
+        window.addEventListener("click", function (e) {
             this.classList.remove("hidden");
             parent.append(this);
         })
     }
-    
+
     resize() {
         this.window.classList.toggle("maximize");
-        
+
     }
-    
+
     close() {
         this.window.style.display = "none";
     }
 }
 
 class WindowItems extends Window {
-    constructor (areaNumber) {
+    constructor(areaNumber) {
         super(areaNumber);
         this.name = "Список приборов";
         this.title.innerHTML = this.name;
@@ -122,45 +406,66 @@ class WindowItems extends Window {
         this.col.innerHTML = "Местоположение";
         this.col.classList.add("legend__item")
         this.legend.append(this.col);
-// переписать в цикл
+        // переписать в цикл
 
         this.fill();
 
     }
 
     fill() {
-        // requestHandler(this, "appliances.json", this.eventsObject);
-        requestHandler("appliances.json");
-        
-        function requestHandler(way) {
-            let requestURL = way;
-            let xhr = new XMLHttpRequest();
+        // local
+        requestHandler(this, jsonAppl, this.eventsObject);
 
-            xhr.open("GET", requestURL);
-            xhr.onerror = function(){console.log("oops")};
-            xhr.send()
+        function requestHandler(object, jsonsmth, eventsObject) {
+            let appliances = jsonsmth;
+            let itemsCount = jsonsmth.length;
+
+            // pageCreator(object, itemsCount, appliances, eventsObject);
+            // let rows = document.querySelectorAll(".row");
+            // for (let k = 0; k < itemsValue; k++) {
+            //     rows[k].querySelector('.id').innerHTML = appliances.items[k].id;
+            //     rows[k].querySelector('.name').innerHTML = appliances.items[k].name;
+            //     rows[k].querySelector('.type').innerHTML = appliances.items[k].type;
+            //     rows[k].querySelector('.location').innerHTML = appliances.items[k].location;
+            // }
         }
 
+        // web
 
-        // function requestHandler(way) {
-        //     let requestWay = way;
-        //     function useRequest(requestWay){
-        //         return fetch(way)
-        //         .then ((response) => {
-        //             return response.json();
-        //         })
-        //         .then ((answer) => {return answer;})
-        //         .catch(() => console.log("an error"))
-        //     }
-    
-        //     useRequest()
+        // requestHandler(this, "appliances.json", this.eventsObject);
+
+        // function requestHandler(object, way, eventsObject) {
+        //     let requestURL = way;
+        //     let xhr = new XMLHttpRequest();
+
+        //     xhr.open("GET", requestURL);
+        //     xhr.responseType = 'json';
+        //     xhr.onerror = function(){console.log("oops")};
+        //     xhr.send();
         // }
+
+        // xhr.addEventListener("load", function() {
+        //     let appliances = request.response;
+        //     let itemsValue = appliances.items.length;
+        //     let rowsPerPage = Math.floor(object.window.querySelector('.window__content').offsetHeight / 50);
+
+        //     pageCreator(object, itemsValue, rowsPerPage, appliances, eventsObject);
+        //     let rows = object.window.querySelectorAll('.row');
+        //     for (let k = 0; k < itemsValue; k++) {
+        //         rows[k].querySelector('.id').innerHTML = appliances.items[k].id;
+        //         rows[k].querySelector('.name').innerHTML = appliances.items[k].name;
+        //         rows[k].querySelector('.type').innerHTML = appliances.items[k].type;
+        //         rows[k].querySelector('.location').innerHTML = appliances.items[k].location;
+        //     }
+        // });
     }
 }
 
+
+
 class WindowEvents extends Window {
-    constructor (areaNumber) {
-        super (areaNumber);
+    constructor(areaNumber) {
+        super(areaNumber);
         this.name = "События";
         this.title.innerHTML = this.name;
     }
@@ -171,22 +476,23 @@ let defaultWindow = new Window(0);
 let defaultWindow3 = new Window(2);
 let itemWindow = new WindowItems(1);
 // let eventsWindow = new WindowEvents(2);
-        
 
-menuStart.addEventListener("click", function() {
+
+menuStart.addEventListener("click", function () {
     menuStart.classList.add("active");
     menuUl.classList.add("active");
     menuLi.classList.add("active");
 });
 
-window.addEventListener("mousemove", function(e) {
-    if ((e.clientX>40) || (e.clientY<(window.innerHeight - 150))) {
+window.addEventListener("mousemove", function (e) {
+    if ((e.clientX > 40) || (e.clientY < (window.innerHeight - 150))) {
         menuStart.classList.remove("active");
         menuUl.classList.remove("active");
         menuLi.classList.remove("active");
     }
 })
 
-addDefault.addEventListener("click", function() {
+addDefault.addEventListener("click", function (e) {
+    e.stopPropagation;
     new Window(0);
 })
