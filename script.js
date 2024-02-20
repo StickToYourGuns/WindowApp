110172bc5eb4b7bc96bc8c7d470efcf2d8a7a07d
Tim <60299355+StickToYourGuns@users.noreply.github.com>
2024-02-21 00:18:28 +0300

    7

--- script.js
+++ script.js
@@ -9,55 +9,55 @@ let jsonAppl = {
             "name": "event 1",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [321, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [165516, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [312165, 20]
         },
         {
             "id": "1",
-            "name": "event 1",
-            "time": "05:17",
+            "name": "event 2",
+            "time": "05:20",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [123123, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [54, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [375, 20]
         },
         {
             "id": "2",
-            "name": "event 1",
-            "time": "05:17",
+            "name": "event 3",
+            "time": "05:21",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [1783, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [3489, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [31571, 20]
         },
         {
             "id": "3",
-            "name": "event 1",
-            "time": "05:17",
+            "name": "event 4",
+            "time": "05:23",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [1354, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [1748, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [1687, 20]
         },
         {
             "id": "4",
-            "name": "event 1",
-            "time": "05:17",
+            "name": "event 5",
+            "time": "05:24",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [1645, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [3452, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [354, 228]
         }
         ]
     },
@@ -71,33 +71,33 @@ let jsonAppl = {
             "name": "event 2",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [1488, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [4615, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [725, 20]
         },
         {
             "id": "1",
             "name": "event 2",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [452, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [562387, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [51064, 20]
         },
         {
             "id": "2",
             "name": "event 2",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [4561, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [6554611, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [5438, 20]
         }
         ]
     },
@@ -111,33 +111,33 @@ let jsonAppl = {
             "name": "event 88",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [35486, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [1337, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [564, 20]
         },
         {
             "id": "1",
             "name": "event 5435",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [32854, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [574, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [456, 20]
         },
         {
             "id": "2",
             "name": "event 12",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [546, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [546, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [35426, 20]
         }
         ]
     },
@@ -151,66 +151,66 @@ let jsonAppl = {
             "name": "event 3",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [412, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [5214, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [1452, 20]
         },
         {
             "id": "1",
             "name": "event 3",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [25146, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [251, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [2135, 20]
         },
         {
             "id": "2",
             "name": "event 3",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [52164, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [2516, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [516, 20]
         },
         {
             "id": "1",
             "name": "event 3",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [32458, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
-            "U2": [100, 10],
-            "U3": [10000, 20]
+            "U1": [2345, 20],
+            "U2": ["СМЕРТЬ"],
+            "U3": [4325, 20]
         },
         {
             "id": "1",
             "name": "event 3",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [34, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [435, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [5164, 20]
         },
         {
             "id": "1",
             "name": "event 3",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [21356, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [251463, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [561, 20]
         }
         ]
     },
@@ -224,33 +224,33 @@ let jsonAppl = {
             "name": "event 4",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [4561, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [145, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [516, 20]
         },
         {
             "id": "1",
             "name": "event 4",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
-            "I3": [100, 10],
-            "U1": [10000, 20],
+            "I2": [516, 20],
+            "I3": ["титьки"],
+            "U1": [51236, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [5621, 20]
         },
         {
             "id": "2",
             "name": "event 4",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [561, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [5621, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [561, 20]
         }
         ]
     },
@@ -264,11 +264,11 @@ let jsonAppl = {
             "name": "event 5",
             "time": "05:17",
             "I1": [100, 10],
-            "I2": [10000, 20],
+            "I2": [561, 20],
             "I3": [100, 10],
-            "U1": [10000, 20],
+            "U1": [5614, 20],
             "U2": [100, 10],
-            "U3": [10000, 20]
+            "U3": [5614, 20]
         }
         ]
     },
@@ -284,9 +284,15 @@ let jsonAppl = {
 
 let menuStart = document.querySelector('.hideBar__start');
 let menuUl = document.querySelector('.hideBar__ul');
-let menuLi = document.querySelector('.hideBar__li');
+let menuLi = document.querySelectorAll('.hideBar__li');
 let menuItems = document.querySelectorAll('.menuItem');
 let addDefault = document.querySelector('.addDefault');
+let addItems = document.querySelector('.addItems');
+let addSessions = document.querySelector('.addSessions');
+let addSettings = document.querySelector('.addSettings');
+let addMap = document.querySelector('.addMap');
+let addersArr = [];
+let adders = addersArr.push(addItems, addSessions, addSettings, addMap);
 
 
 let elements = document.querySelectorAll('.workspace__area');
@@ -367,8 +373,8 @@ class Window {
     }
 
     hide() {
-        let parent = this.window.parentNode;
         let window = this.window;
+        let parent = this.window.parentNode;
         window.classList.add("hidden");
         document.querySelector(".hideBar").append(window);
         window.addEventListener("click", function (e) {
@@ -393,6 +399,7 @@ class WindowItems extends Window {
         this.name = "Список приборов";
         this.title.innerHTML = this.name;
         this.eventsObject = new WindowEvents(areaNumber);
+        this.window.classList.add("items");
 
         for (let i = 0; i < 4; i++) {
             let col = document.createElement("p");
@@ -508,13 +515,12 @@ class WindowItems extends Window {
     }
 }
 
-
-
 class WindowEvents extends Window {
     constructor(areaNumber) {
         super(areaNumber);
         this.name = "События";
         this.title.innerHTML = this.name;
+        this.window.classList.add("events");
 
         for (let i = 0; i < 8; i++) {
             let col = document.createElement("p");
@@ -548,33 +554,140 @@ class WindowEvents extends Window {
             this.legend.append(col);
         }
     }
-    fill(jsonAppl, id) {
-
+    fill(jsonObj, id) {
+        console.log(jsonObj)
+        this.content.innerHTML = null;
+        let itemsEventCount = jsonObj.items[id].events.length;
+        for (let i = 0; i < itemsEventCount; i++) {
+            let row = document.createElement("div");
+            row.classList.add('row');
+            for (let j = 0; j < 9; j++) {
+                let col = document.createElement("div");
+                col.classList.add("col__event");
+                switch (j) {
+                    case 0:
+                        col.classList.add("id")
+                        col.innerHTML = jsonObj.items[id].events[i].id;
+                        break;
+                    case 1:
+                        col.classList.add('name');
+                        col.innerHTML = jsonObj.items[id].events[i].name;
+                        break;
+                    case 2:
+                        col.classList.add('time');
+                        col.innerHTML = jsonObj.items[id].events[i].time;
+                        break;
+                    case 3:
+                        col.classList.add('I1');
+                        col.innerHTML = jsonObj.items[id].events[i].I1;
+                        break;
+                    case 4:
+                        col.classList.add('I2');
+                        col.innerHTML = jsonObj.items[id].events[i].I2;
+                        break;
+                    case 5:
+                        col.classList.add('I3');
+                        col.innerHTML = jsonObj.items[id].events[i].I3;
+                        break;
+                    case 6:
+                        col.classList.add('U1');
+                        col.innerHTML = jsonObj.items[id].events[i].U1;
+                        break;
+                    case 7:
+                        col.classList.add('U2');
+                        col.innerHTML = jsonObj.items[id].events[i].U2;
+                        break;
+                    case 8:
+                        col.classList.add('U3');
+                        col.innerHTML = jsonObj.items[id].events[i].U3;
+                        break;
+                }
+                row.append(col);
+                this.content.append(row);
+            }
+        }
     }
 }
 
+class WindowSessions extends Window {
+    constructor(areaNumber) {
+        super(areaNumber);
+        this.name = "Сеансы";
+        this.title.innerHTML = this.name;
+        this.window.classList.add("sessions");
+    }
+}
 
-// let defaultWindow = new Window(0);
-// let defaultWindow3 = new Window(2);
-let itemWindow = new WindowItems(1);
-// let eventsWindow = new WindowEvents(2);
+class WindowSettings extends Window {
+    constructor(areaNumber) {
+        super(areaNumber);
+        this.name = "Настройки";
+        this.title.innerHTML = this.name;
+        this.window.classList.add("settings");
+    }
+}
 
+class WindowMap extends Window {
+    constructor(areaNumber) {
+        super(areaNumber);
+        this.name = "Карта";
+        this.title.innerHTML = this.name;
+        this.content.innerHTML = `<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A12ddf9b2150c5f7e2182153169dbcce517198d89d1b99de24d6ae9f7e1087bbd&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>`
+        this.window.classList.add("map");
+    }
+}
+
+let item2 = new WindowItems(1);
+let item1 = new WindowItems(0);
+// setTimeout(function(){
+// }, 5000);
+// let sessions1 = new WindowSessions(1);
+let settings1 = new WindowSettings(2);
+let map1 = new WindowMap(2);
 
 menuStart.addEventListener("click", function () {
     menuStart.classList.add("active");
     menuUl.classList.add("active");
-    menuLi.classList.add("active");
+    menuLi.forEach(function (item) {
+        item.classList.add("active");
+    })
 });
 
 window.addEventListener("mousemove", function (e) {
-    if ((e.clientX > 40) || (e.clientY < (window.innerHeight - 150))) {
+    if ((e.clientX > 40) || (e.clientY < (window.innerHeight - 210))) {
         menuStart.classList.remove("active");
         menuUl.classList.remove("active");
-        menuLi.classList.remove("active");
+        menuLi.forEach(function (item) {
+            item.classList.remove("active");
+        })
     }
 })
 
-addDefault.addEventListener("click", function (e) {
-    e.stopPropagation;
-    new Window(0);
-})
+for(let i=0; i<addersArr.length; i++) {
+    switch (i) {
+        case 0:
+            addersArr[i].addEventListener("click", function() {
+                let addItem = new WindowItems(0);
+            })
+            break;
+        case 1:
+            addersArr[i].addEventListener("click", function() {
+                let addSesion = new WindowSessions(1);
+            })
+            break;
+        case 2:
+            addersArr[i].addEventListener("click", function() {
+                let addSettinigs = new WindowSettings(2);
+            })
+            break;
+        case 3:
+            addersArr[i].addEventListener("click", function() {
+                let addMap = new WindowMap(2);
+            })
+            break;
+    }
+}
+
+// addItems.addEventListener("click", function (e) {
+//     let addItem = new WindowItems(0);
+// })
